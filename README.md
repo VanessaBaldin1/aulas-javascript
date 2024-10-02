3247231238983f745ca8b75174c13e330907efb6
VanessaBaldin1 <vanbaldin08@gmail.com>
2024-10-02 09:32:54 -0300

    Utilização Matriz Arrays

--- README.md
+++ README.md
@@ -38,4 +38,6 @@ Arrays são estruturas de dados **indexados**, também conhecidos como **vetores
 
 Na prática, um array é uma lista de dados sequenciais, e cada dado é armazenado em uma posição/índice do array.
 
+**Obs.:** a contagem de elementos do array **sempre inicia em zero**
+
 
--- js/03-arrays.js
+++ js/03-arrays.js
@@ -8,4 +8,37 @@ console.log(alunos);
 // Acessando um elemento especificodo array
 console.log(` ${alunos[1]} é o vocalista da banda Queen!🤘`);
 
+/* Mini-exercícios
+
+1) Crie um novo array contendo o nome de 7 coisas que você gosta (artista, música, livro, comida... sei lá, inventa!*/
+
+let curiosidades = ["Fredie Mercury", "Luis Miguel", "We are de campions", "UX Design elementos", "Pizza","PUGs","Viajar","Natal"];
+
+
+/*2) Em seguida, mostre no console um frase personalizada indicando o nome do segundo, quinto e do sétimo  elemento do array. Use concatenação Ou template string.*/
+
+console.log(`Amo as músicas do cantor ${curiosidades[1]} como sou fã de ${curiosidades[4]}, quando estou na estrada ${curiosidades[6]}`);
+
+/* Arrays como matriz de 2 dimensões */
+
+const  tecnologias = [
+    ["HTML5","CSS3","JavaScript"],
+    ["Figma", "Photoshop"],
+    ["PHP", "Node.js","SQL","Express", ["Apache", "IIS"]]
+];
+
+// console.log(tecnologias);
+
+
+console.log(tecnologias[0][2]); //JavaScript
+console.log(tecnologias[1][0]); //Figma
+console.log(tecnologias[2][3]); //Express
+console.log(tecnologias[0][0]); //HTML5
+console.log(tecnologias[2][4][0]); // Apache
+
+
+
+
+
+
 
